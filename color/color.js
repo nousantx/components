b357cc138397cd6d0e45bb676098bbdb0b88b1b6
nousantx <nousantx@gmail.com>
2024-04-03 15:21:25 +0700

    feat: Add color generation function

--- color/color.js
+++ color/color.js
@@ -1,1 +1,32 @@
-const color = [];
+const cssColor = [
+  "primary",
+  "accent",
+  "info",
+  "success",
+  "warning",
+  "danger",
+  "neutral",
+];
+
+const color = {};
+function makeColor(colors) {
+  colors.forEach((category) => {
+    for (let i = 100; i <= 900; i += 100) {
+      // text color generation
+      const textClassName = `.text-${category}-${i}`;
+      const textColorValue = `tc-[${category}-${i}]`;
+      color[textClassName] = textColorValue;
+      // button background color generation
+      const bgClassName = `.bg-${category}-${i}`;
+      const bgColorValue = `back-c-[${category}-${i}]`;
+      color[bgClassName] = bgColorValue;
+      // button background color generation
+      const btnClassName = `.btn-${category}-${i}`;
+      const btnColorValue = `back-c-[${category}-${i}]`;
+      color[btnClassName] = btnColorValue;
+    }
+  });
+  makeStyles(color);
+}
+// console.log(color);
+makeColor(cssColor);
